597919ea925280b5a3c82433945838b1e2f5919f
JuYeong98 <77095811+JuYeong98@users.noreply.github.com>
2021-06-23 05:19:20 +0900

    first commit

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 const pool = mysql.createPool({
-    connectionLimit: 10,
+    connectionLimit: 15,
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_ROOT_PASSWORD,
